3fdb59c9852228766948c411c9928fed320b7922
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-02-19 12:55:50 +0100

    added more rrd files specified at end of file as glob pattern

--- rrd2influx.pl
+++ rrd2influx.pl
@@ -4,10 +4,17 @@ use strict;
 use autodie;
 
 use Data::Dump qw(dump);
+my $debug = $ENV{DEBUG};
 
 my $url = "http://10.60.0.92:8086/write?db=munin_archive";
 
-my @files = glob '/var/lib/munin/*/*fail2ban*.rrd';
+my @files;
+while(<DATA>) {
+	chomp;
+	my @add = glob $_;
+	push @files, @add;
+	warn "# $_ -> ", $#add + 1, " rrd files\n" if $debug;
+}
 @files = @ARGV if @ARGV;
 
 my $influx_path = '/dev/shm/rrd2influx';
@@ -18,7 +25,7 @@ foreach my $file ( @files ) {
 	$id =~ s{/var/lib/munin/}{} || die "can't strip path from: $id";
 	my ($group,$host,$plugin,$name,undef) = split(/[\/-]/,$id);
 	my @a = split(/[\/-]/,$id);
-	warn "# file = $file ", dump(\@a);
+	warn "# file = $file ", dump(\@a) if $debug;
 
 	my $cf = '_UNKNOWN';
 	my $t_off = 0; # spread average/min/max
@@ -31,17 +38,22 @@ foreach my $file ( @files ) {
 			$t_off++; # multiple values for same time offset (kind of)
 		} elsif ( m{/ (\d+) --> <row><v>(.+)</v></row>} ) {
 			if ( $2 + 0 != $2 ) {
-				warn "SKIP: $_";
+				warn "SKIP: $_" if $debug;
 				next;
 			}
 			my ( $t, $v ) = ( $1, $2 + 0 );
 			print $influx "$plugin,group=$group,host=$host,plugin=$plugin,cf=$cf,name=$name $cf=$v ", $t * 1000000000 + $t_off, "\n";
 		} else {
-			warn "## $_";
+			warn "## $_" if $debug;
 		}
 	}
 	close ($influx);
-	system "curl -i -XPOST $url --data-binary \@$influx_path";
+	system "curl -q -XPOST $url --data-binary \@$influx_path";
 
 }
 
+__DATA__
+/var/lib/munin/*/*-fail2ban-*.rrd
+/var/lib/munin/net.ffzg/deenes-amavis-*.rrd
+/var/lib/munin/net.ffzg/deenes-clamav-*.rrd
+/var/lib/munin/*/*-postfix_*.rrd
